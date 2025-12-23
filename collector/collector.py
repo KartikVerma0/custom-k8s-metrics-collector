f759c8bbb0f9141243713977c86282799f77f8d8
Kartik Verma <kartik.verma@73strings.com>
2025-12-23 23:16:31 +0530

    Enhance custom metrics collector functionality
    - Updated .gitignore to include .env files.
    - Moved collector_ns.yaml to root folder.
    - Modified collector.py to support argument parsing for development mode, allowing local environment variable loading.
    - Implemented metrics sending to a metrics processor service with error handling.
    - Created requirements_collector_dev.txt for development dependencies including python-dotenv.

--- collector/collector.py
+++ collector/collector.py
@@ -2,16 +2,34 @@ from kubernetes import client, config
 import os
 import logging
 import time
+import argparse
+import requests
+
+from dotenv import load_dotenv
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-config.load_incluster_config()
+def setup_arg_parsing():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dev",action="store_true", help="Enables dev mode")
+    args = parser.parse_args()
+    return args
+
+args = setup_arg_parsing()
+
+if args.dev == True:
+    load_dotenv()
+    config.load_kube_config()
+else:
+    config.load_incluster_config()
 
 def metrics_resolution_validator():
     _metrics_resolution_str = os.getenv("METRICS_RESOLUTION_TIME")
 
     if _metrics_resolution_str == None:
+        logger.error("No metrics resolution time provided.")
+        logger.info("Setting metrics resolution to 15s")
         return 15
 
     try:
@@ -36,15 +54,30 @@ def scrap_node_metrics():
         version = "v1beta1"
         plural = "nodes"
         api_response = api_instance.list_cluster_custom_object(group, version, plural)
-        logger.debug(api_response)
+        send_metrics_to_metrics_processor(api_response)
+
+def get_metrics_processor_service_url():
+    if (args.dev == False):
+        service_name = os.getenv("METRICS_PROCESSOR_SERVICE") if os.getenv("METRICS_PROCESSOR_SERVICE") else "metrics_processor"
+        service_namespace = os.getenv("METRICS_PROCESSOR_SERVICE_NAMESPACE") if os.getenv("METRICS_PROCESSOR_SERVICE_NAMESPACE") else "custom-metrics-collection"
+        return f"http://{service_name}.{service_namespace}.svc.cluster.local"
+    else:
+        return os.getenv("METRICS_PROCESSOR_URL")
+
+def send_metrics_to_metrics_processor(metrics):
+    url = get_metrics_processor_service_url() + "/node_metrics"
+    try:
+        response = requests.post(url, json=metrics, timeout=10)
+        print(response)
+        response.raise_for_status()
+    except:
+        logger.error("Problem sending metrics data to metrics_processor")
 
 def scheduler():
+    metrics_resolution = metrics_resolution_validator()
     while True:
         scrap_node_metrics()
-        metrics_resolution = metrics_resolution_validator()
         time.sleep(metrics_resolution)
-    
+
 if __name__ == "__main__":
     scheduler()
-
-
